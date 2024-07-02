a0292c29f75f400568928cb199c4d4400964594e
Sören <69844028+soerenarlt@users.noreply.github.com>
2024-07-02 17:11:41 +0200

    construction codes + plotting

--- README.md
+++ README.md
@@ -1,32 +1,5 @@
-## Instructions for repeating results from our paper
+## Code for generating and validating experimental setups described in our paper
 
-To repeat the inverse-design of the experimental setups, install the [pytheus package](https://github.com/artificial-scientist-lab/PyTheus)
-```
-pip install pytheusQ
-```
-### 4-dimensional 4-particle GHZ state
-Run
-```
-pytheus run 4d4pGHZ.json
-```
-A directory `output` is created were results are stored.
+Run the python file `setups.py` to generate the setups, plot a simple representation of the experimental setups (saved as png files), and print the resulting quantum states.
 
-The parameters of the computation can be changed in `4d4pGHZ.json`
-
-
-### Non-local creation of a 4-dimensional Bell state
-Run
-```
-pytheus run nonlocal4dBell.json
-```
-A directory `output` is created were results are stored.
-
-The parameters of the computation can be changed in `nonlocal4dBell.json`
-
-### Plotting an experimental setup
-
-Copy the `graph` dictionary found in a `clean` result and paste into `plotscript.py`.
-
-This automatically produces two files `path_identity.png` and `bulk_optics_path_encoding.png`.These are first drafts for the final experiment. For instructions for how to translate the graph result by hand or adjust the automatically translated setup, refer to our [accompanying paper]( 	
-https://doi.org/10.48550/arXiv.2210.09980).
-
+For each example, the value of the variable k determines the number of times the building block (multi-particle emitter emulated by pair-sources) is emitted. Increasing this number in the code generates larger setups with higher dimensions.
--- setups.py
+++ setups.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import matplotlib.pyplot as plt
 
 ###### FUNCTIONS TO COMPUTE THE STATE OF A PHOTONIC QUANTUM CIRCUIT ######
 def readSetup(setup):
@@ -52,11 +53,28 @@ def buildState(setup):
     if ancilla_photons > 0: state_str += '('
     for key, value in state_dict.items():
         if value > 0: state_str += '+'
-        state_str += f'{value:.3f}|{key[:-ancilla_photons]}⟩'
-    if ancilla_photons > 0: state_str += f')⊗|{ket[-ancilla_photons:]}>'
+        state_str += f'{value:.3f}|{key[:path_count-ancilla_photons]}⟩'
+    if ancilla_photons > 0: state_str += f')⊗|{ket[-ancilla_photons:]}⟩'
     print(state_str)
     return state_str
 
+def plotSetup(pair_sources, file_name='setup.png'):
+    path_count = len(set(sum((source[:2] for source in pair_sources), [])))
+    #plot path_count vertical lines, each line represents a path
+    plt.figure(figsize=(2+0.5*path_count, 2+0.2*len(pair_sources)))
+    for i in range(path_count):
+        plt.axvline(x=i, color='black')
+    colors = ['red', 'green', 'blue', 'orange', 'pink', 'brown'] + list(plt.cm.viridis(range(256)))
+    for ii, source in enumerate(pair_sources):
+        plt.plot([source[0], source[0]+(source[1]-source[0])/2], [-ii, -ii], 'o-', color=colors[source[2]], linewidth=8, markersize=0)
+        plt.plot([source[1], source[0]+(source[1]-source[0])/2], [-ii, -ii], 'o-', color=colors[source[3]], linewidth=8, markersize=0)
+        plt.plot(source[1], -ii, 'k*', markersize=10) #mark the path that the photon is sent to with a star
+        plt.plot(source[0], -ii, 'k*', markersize=10) #mark the path that the photon is sent with a star
+        if source[4] < 0: plt.plot(source[0]+(source[1]-source[0])/2, -ii, 'kd', markersize=8, markerfacecolor='white') #mark negative amplitudes with white diamond
+    plt.yticks([])
+    plt.xticks(range(path_count))
+    plt.xlabel('Paths')
+    plt.savefig(file_name)
 
 # adding a photon pair source to the list of sources
 # emitting a pair of photons in mode1 and mode2 to path1 and path2 with a relative amplitude
@@ -65,9 +83,8 @@ def C(path1, path2, mode1, mode2, amplitude):
 
 
 ###### EXAMPLE: d-DIMENSIONAL GHZ STATE ######
-
-# setup of pair-sources for 3+N dimensional GHZ state (N>=0)
-N = 3
+# setup of pair-sources for 3+k dimensional GHZ state (k>=0) with 4 particles, requires 4*k ancilla photons
+k = 1
 pair_sources = []
 
 C(0,1,0,0,1) #function to create a pair source sending a pair of photons to the first two paths (0 and 1) with mode 0
@@ -76,7 +93,7 @@ C(0,2,1,1,1)
 C(1,3,1,1,1)
 C(0,3,2,2,1)
 C(1,2,2,2,1)
-for i in range(N): #for higher dimensions, we need to create more pair sources and ancilla detectors
+for i in range(k): #for higher dimensions, we need to create more pair sources and ancilla detectors
     dim = 3 + i
     anc1 = 4 + 4*i
     anc2 = 5 + 4*i
@@ -97,19 +114,18 @@ for i in range(N): #for higher dimensions, we need to create more pair sources a
     C(anc3, 2, dim, dim, -1)
 
 # readSetup(pair_sources) #uncomment to print the setup
-print(f'Building {3+N} dimensional GHZ state')
+print(f'Buidling setup for {3+k} dimensional GHZ state (4 particles + {4*k} ancilla photons)')
 print(f'Setup has {len(pair_sources)} pair sources')
 buildState(pair_sources) #output the state computed from the setup
-
 print('\n')
+plotSetup(pair_sources, 'GHZ_setup.png')
 
 ###### EXAMPLE: NON-LOCAL CREATION OF d-DIMENSIONAL BELL STATE ######
-
-# setup for non-local creation of 2N dimensional Bell state (N>=1)
-N = 2
+# setup for non-local creation of 2k dimensional Bell state (k>=1), requires 2*k ancilla photons
+k = 2
 pair_sources = []
 
-for i in range(N):
+for i in range(k):
     dim1 = 2*i
     dim2 = 2*i + 1
     anc1 = 2 + 2*i
@@ -122,6 +138,128 @@ for i in range(N):
     C(1, anc1, dim2, 0, 1)
 
 # readSetup(pair_sources) #uncomment to print the setup
-print(f'Building {2*N} dimensional Bell state')
+print(f'Building {2*k} dimensional Bell state (2 particles + {2*k} ancilla photons)')
 print(f'Setup has {len(pair_sources)} pair sources')
 buildState(pair_sources) #output the state computed from the setup
+plotSetup(pair_sources, 'Bell_setup.png')
+print('\n')
+
+##### EXAMPLE: 6 particle source ######
+# setup of pair-sources for 2+k dimensional GHZ state (k>=0) with 6 particles, requires 6*k ancilla photons
+k = 1
+pair_sources = []
+
+C(0,1,0,0,1)
+C(2,3,0,0,1)
+C(4,5,0,0,1)
+C(1,2,1,1,1)
+C(3,4,1,1,1)
+C(0,5,1,1,1)
+
+for i in range(k):
+    dim = 2 + i
+    anc1 = 6 + 6*i
+    anc2 = 7 + 6*i
+    anc3 = 8 + 6*i
+    anc4 = 9 + 6*i
+    anc5 = 10 + 6*i
+    anc6 = 11 + 6*i
+    
+    offset = 7
+
+    C(anc1, anc5, dim, dim, -1)
+    C(anc1, anc4, dim, dim, 1)
+    C(anc1, anc6, dim, dim, -1)
+    C(anc1, 7-offset, dim, dim, 1)
+    C(anc1, 8-offset, dim, dim, -1)
+    C(anc2, anc4, dim, dim, 1)
+    C(anc2, anc5, dim, dim, -1)
+    C(anc2, anc6, dim, dim, -1)
+    C(anc2, 7-offset, dim, dim, -1)
+    C(anc2, 8-offset, dim, dim, 1)
+    C(anc2, 12-offset, dim, dim, -1)
+    C(anc3, anc4, dim, dim, -1)
+    C(anc3, anc5, dim, dim, 1)
+    C(anc3, 12-offset, dim, dim, -1)
+    C(anc4, 9-offset, dim, dim, 1)
+    C(anc4, 10-offset, dim, dim, 1)
+    C(anc5, 9-offset, dim, dim, 1)
+    C(anc5, 10-offset, dim, dim, 1)
+    C(anc6, 11-offset, dim, dim, 1)
+
+# readSetup(pair_sources) #uncomment to print the setup
+print(f'Buidling setup for {2+k} dimensional GHZ state (6 particles + {6*k} ancilla photons)')
+print(f'Setup has {len(pair_sources)} pair sources')
+buildState(pair_sources) #output the state computed from the setup
+plotSetup(pair_sources, 'GHZ6_setup.png')
+print('\n')
+
+
+###### EXAMPLE: 8 particle source ######Œ
+# setup of pair-sources for 2+k dimensional GHZ state (k>=0) with 8 particles, requires 8*k ancilla photons
+k = 1
+pair_sources = []
+
+C(0,1,0,0,1)
+C(2,3,0,0,1)
+C(4,5,0,0,1)
+C(6,7,0,0,1)
+C(1,2,1,1,1)
+C(3,4,1,1,1)
+C(5,6,1,1,1)
+C(0,7,1,1,1)
+
+for i in range(k):
+    dim = 2 + i
+    anc1 = 8 + 8*i
+    anc2 = 9 + 8*i
+    anc3 = 10 + 8*i
+    anc4 = 11 + 8*i
+    anc5 = 12 + 8*i
+    anc6 = 13 + 8*i
+    anc7 = 14 + 8*i
+    anc8 = 15 + 8*i
+    
+    offset = 9
+
+    C(anc1, anc5, dim, dim, 1)
+    C(anc1, anc6, dim, dim, -1)
+    C(anc1, anc8, dim, dim, -1)
+    C(anc1, 11-offset, dim, dim, 1)
+    C(anc1, 14-offset, dim, dim, -1)
+
+    C(anc2, anc3, dim, dim, 1)
+    C(anc2, anc6, dim, dim, 1)
+    C(anc2, anc8, dim, dim, 1)
+    C(anc2, 10-offset, dim, dim, 1)
+    C(anc2, 15-offset, dim, dim, -1)
+
+    C(anc3, anc4, dim, dim, -1)
+    C(anc3, 9-offset, dim, dim, -1)
+    C(anc3, 16-offset, dim, dim, -1)
+
+    C(anc4, anc6, dim, dim, -1)
+    C(anc4, anc8, dim, dim, -1)
+    C(anc4, 10-offset, dim, dim, 1)
+    C(anc4, 15-offset, dim, dim, -1)
+
+    C(anc5, anc7, dim, dim, -1)
+    C(anc5, 13-offset, dim, dim, 1)
+    C(anc5, 16-offset, dim, dim, 1)
+
+    C(anc6, anc7, dim, dim, 1)
+    C(anc6, 12-offset, dim, dim, -1)
+    
+    C(anc7, anc8, dim, dim, 1)
+    C(anc7, 11-offset, dim, dim, 1)
+    C(anc7, 14-offset, dim, dim, -1)
+
+    C(anc8, 9-offset, dim, dim, 1)
+    C(anc8, 12-offset, dim, dim, 1)
+    C(anc8, 13-offset, dim, dim, 1)
+
+# readSetup(pair_sources) #uncomment to print the setup
+print(f'Buidling setup for {2+k} dimensional GHZ state (8 particles + {8*k} ancilla photons)')
+print(f'Setup has {len(pair_sources)} pair sources')
+buildState(pair_sources) #output the state computed from the setup
+plotSetup(pair_sources, 'GHZ8_setup.png')
